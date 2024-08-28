1b8472a07428f788ce8da33615d192e18837331a
rushiikeshshinde <rushiikeshshiinde@gmail.com>
2024-08-28 11:41:35 +0530

    Updated: Changes in ConstructorChaining.java

--- src/Level_3_AdvancedOOPs/ConstructorChaining.java
+++ src/Level_3_AdvancedOOPs/ConstructorChaining.java
@@ -5,15 +5,19 @@ class Chaining {
     int x,y;
 
     public Chaining() {
-        System.out.println("Default Constructor Chaining Constructor");
+        this(20);
     }
     public Chaining(int x) {
         this.x = x;
+        System.out.println("This is single constructor chaining");
         System.out.println(this.x);
     }
     public Chaining(int x, int y) {
+        this();
         this.x = x;
-        this.y = y;
+
+
+        System.out.println("This is double constructor chaining");
 
         System.out.println(this.x);
         System.out.println(this.y);
